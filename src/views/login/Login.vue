8463a02cc0c9fe45270e2a15556c8620abcce88d
h <710543268@qq.com>
2022-04-13 18:21:52 +0800

    父子组件传参

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,20 +1,46 @@
 <template>
-  <div>用户名:{{ mainStore.name }}<br />长度:{{ mainStore.nameLength }}</div>
+  <div class="login">
+    <Menu title="我是menu" @onTitle="onTitle" ref="menus"></Menu>
+  </div>
   <hr/>
-  <n-button @click="updateName">修改store中的name</n-button>
+  <ul>
+    <li v-for="(item,index) in data.list" :key="index">
+      {{ item }}
+    </li>
+  </ul>
+  <n-button @click="updateName">
+    <!--    <a href="https://pic3.zhimg.com/v2-58d652598269710fa67ec8d1c88d8f03_r.exe" download="jpg">修改store中的name</a>-->
+    修改store中的name
+  </n-button>
 </template>
 
 <script setup lang="ts">
-import { NButton } from "naive-ui"
+import {NButton} from 'naive-ui'
 import {useMainStore} from '@/store/main'
+import Menu from '@/components/Menu.vue'
+import {reactive, ref} from 'vue'
+
 const mainStore = useMainStore()
 const updateName = () => {
   // $patch 修改 store 中的数据
   mainStore.$patch({
     name: '名称被修改了,nameLength也随之改变了'
   })
+  //window.open('https://pic3.zhimg.com/v2-58d652598269710fa67ec8d1c88d8f03_r.jpg','haohao')
 }
-console.log(import.meta.env.VITE_APP_WEB_URL)
+const menus = ref(null)
+console.log(menus.value)
+type Menu = {
+  list?: number[],
+  addList?: number[]
+}
+const data = reactive<Menu>({
+  list: [1, 2, 3]
+})
+const onTitle = (dataList: number[]) => {
+  data.list = dataList
+  console.log(data.list)
+}
 </script>
 
 <style scoped>
